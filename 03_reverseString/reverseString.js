6ed59687577d53059e83b8e75f34d06fa515d0b5
Shinryo <157136484+5h1nry0@users.noreply.github.com>
2024-02-04 11:34:32 -0500

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let newArray = []
+    const splitString = text.split("");
+    for (const char of splitString) {
+      newArray.unshift(char)
+    };
+    let newString = newArray.join('');
+    return newString
 };
 
 // Do not edit below this line
